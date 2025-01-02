75709527e544a93e81e5727fe7fd02deca46ce2d
LeeMin-hyeong <lmhstrar070@naver.com>
2025-01-02 23:21:42 +0900

    utils update

--- src/app/actions/utils.ts
+++ src/app/actions/utils.ts
@@ -7,13 +7,3 @@ export function hasPermission(
 ) {
   return !!_.intersection(requires, permissions).length;
 }
-
-export function checkIfSoldierHasPermission(value: number, scope: Permission[]) {
-  if (scope.includes('Admin') || scope.includes('Commander')) {
-    return { message: null };
-  }
-  if (!scope.includes('Nco')) {
-    return { message: '상벌점을 줄 권한이 없습니다' };
-  }
-  return { message: null };
-}
