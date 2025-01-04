537cef956b8d586d238a824020d4b34548477d0d
LeeMin-hyeong <lmhstrar070@naver.com>
2025-01-04 16:18:07 +0900

    mobile compatible

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,7 @@ export default async function RootLayout({
       <head>
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
         <style>{`
           html, body {
             margin: 0;
