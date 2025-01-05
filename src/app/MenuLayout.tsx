792b004cae3b78a7613dffadc080deac852d9d17
LeeMin-hyeong <lmhstrar070@naver.com>
2025-01-05 23:14:44 +0900

    collaspe slider when click outside of slider

--- src/app/MenuLayout.tsx
+++ src/app/MenuLayout.tsx
@@ -256,7 +256,7 @@ export function MenuLayout({
                 {renderTitle(pathname)}
               </p>
             </Layout.Header>
-            <Layout.Content>{children}</Layout.Content>
+            <Layout.Content onClick={useCallback(() => setCollapsed((state) => !state ? !state : state), [])}>{children}</Layout.Content>
             <Layout.Footer style={{ textAlign: 'center' }}>
               <span className='text-black font-bold'>
                 ©{new Date().getFullYear()} 키보드워리어
